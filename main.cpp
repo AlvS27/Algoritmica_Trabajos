def78ed81229da1cccb9c7bc7407e4de1035759d
AlvS27 <tiago.l.jackson@gmail.com>
2024-09-10 20:37:43 -0700

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,16 @@
+/* Programa: Nombre */
+/* #include<stdio.h> */
+/* #include<conio.h> */
+#include<cstdio>
+
+int main()
+{
+    char nom[30], ape[30];
+
+    printf("Intodusca su Nombre: "); scanf("%s",&nom);
+    printf("Intodusca su Apellido: "); scanf("%s",&ape);
+    printf("Hola %s %s, buenos d%cas",nom,ape, 161);
+   /*getch();*/
+
+    return 0;
+}
