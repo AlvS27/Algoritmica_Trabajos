51910865ccf9c6e03561eb7193ce1840c636969c
AlvS27 <tiago.l.jackson@gmail.com>
2024-09-20 14:28:10 -0700

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    char nom[30],ape[30];
+    cout << "Introdusca su Nombre: ";
+    cin >> nom;
+    cout << "Introdusca su Apellido: ";
+    cin >> ape;
+
+    cout << "Hola" << ", buenos dias " << nom << " " << ape << endl;
+    return 0;
+}
