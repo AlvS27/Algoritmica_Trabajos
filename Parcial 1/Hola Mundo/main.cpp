fead9ebc10e5a6b984209dc5c15b59a9647b2e6b
AlvS27 <tiago.l.jackson@gmail.com>
2024-10-06 15:37:51 -0700

    Add files via upload

---
+++ Parcial 1/Hola Mundo/main.cpp
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main()
+{
+    /*Comentario*/
+    printf("hola mundo");
+    printf("\nHOLA MUNDO"); /*Salto de linea*/
+    printf("\nX\nY");
+    printf("\n\nHoLa MuNdO\a");
+    printf("\n\n\n \"\Hola Mundo?\"");
+}
