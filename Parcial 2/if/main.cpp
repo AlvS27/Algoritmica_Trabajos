115b9a2cda5c5027b2aeb5f93f0f47efeb385743
AlvS27 <tiago.l.jackson@gmail.com>
2024-10-17 01:28:21 -0700

    Add files via upload

---
+++ Parcial 2/if/main.cpp
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<conio.h>
+
+int main(){
+    float cal1,cal2,cal3,prom;
+    printf("Inserte su Primera Calificacion: "); scanf("%f",&cal1);
+    printf("Inserte su Segunda Calificacion: "); scanf("%f",&cal2);
+    printf("Inserte su Tercera Calificacion: "); scanf("%f",&cal3);
+
+    prom=(cal1+cal2+cal3)/3;
+    if(prom>=7){
+           printf("\nAPROBADO");
+    }
+    else{
+            printf("\nREPROBADO >:(");
+    }
+
+    getch();
+    return 0;
+}
