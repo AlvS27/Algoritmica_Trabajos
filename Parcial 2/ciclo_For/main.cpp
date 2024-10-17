ce19e57ff1584d5a9b497533ad6d88c296adbda2
AlvS27 <tiago.l.jackson@gmail.com>
2024-10-17 01:12:41 -0700

    Add files via upload

---
+++ Parcial 2/ciclo_For/main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int num;
+    /*
+    for(num=1;num<=10;num++){
+    cout << num << endl;
+    }
+    */
+    for(num=10;num>=1;num--){
+    cout << num << endl;
+    }
+    return 0;
+}
