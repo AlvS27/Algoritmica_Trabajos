529bfa249d550b339ec439d04f5568acb27166c2
AlvS27 <tiago.l.jackson@gmail.com>
2024-10-17 03:56:03 -0700

    Add files via upload

---
+++ Parcial 2/ciclo_While/main.cpp
@@ -0,0 +1,25 @@
+/*Programa: Double*/
+#include<stdio.h>
+#include<conio.h>
+int main(){
+    double num,suma,pro;
+    num= 0;
+    pro= 1;
+
+    printf("Escribe un Numero diferente de 0: "); scanf("%lf",&num);
+    if(num!=0){
+    while(pro++){
+            printf("\nEscribe otro Numero diferente de 0: "); scanf("%lf",&pro);
+            suma= (num+pro)+suma;
+            printf("La suma es: %.2lf\n",suma);
+            num=0;
+    }
+      if(pro!=0){
+                    printf("\nNig >:(");
+            }
+    }
+    else{
+        printf("\nNig >:(");
+
+    }
+}
