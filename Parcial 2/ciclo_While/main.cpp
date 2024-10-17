bf4251ee8fb067a0f442b2a49fde5763004c427b
AlvS27 <tiago.l.jackson@gmail.com>
2024-10-17 01:19:10 -0700

    Add files via upload

---
+++ Parcial 2/ciclo_While/main.cpp
@@ -0,0 +1,20 @@
+/*Programa: ciclo_While*/
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int num;
+    num= 1;
+    while(num<=10){
+        cout << num << endl;
+        num ++;
+    }
+    /*num= 10;
+    while(num>=0){
+        cout << num << endl;
+        num --;
+    }*/
+    return 0;
+}
